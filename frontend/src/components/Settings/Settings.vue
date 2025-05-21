4d3ecfa4cc36f34a0c896242946b7aa94b292044
Shariq Ansari <sharique.rik@gmail.com>
2025-05-21 15:40:37 +0530

    fix: renamed invite members to invite agent
    (cherry picked from commit 5c7f835e4c2d6400302c3466ab0b8c9d655bf274)

--- frontend/src/components/Settings/Settings.vue
+++ frontend/src/components/Settings/Settings.vue
@@ -54,7 +54,7 @@ import PhoneIcon from '@/components/Icons/PhoneIcon.vue'
 import Email2Icon from '@/components/Icons/Email2Icon.vue'
 import Agents from '@/components/Settings/Agents.vue'
 import GeneralSettings from '@/components/Settings/GeneralSettings.vue'
-import InviteMemberPage from '@/components/Settings/InviteMemberPage.vue'
+import InviteAgentPage from '@/components/Settings/InviteAgentPage.vue'
 import ProfileSettings from '@/components/Settings/ProfileSettings.vue'
 import WhatsAppSettings from '@/components/Settings/WhatsAppSettings.vue'
 import ERPNextSettings from '@/components/Settings/ERPNextSettings.vue'
@@ -116,9 +116,9 @@ const tabs = computed(() => {
           condition: () => isManager(),
         },
         {
-          label: __('Invite User'),
-          icon: 'user-plus',
-          component: markRaw(InviteUserPage),
+          label: __('Invite Agent'),
+          icon: InviteIcon,
+          component: markRaw(InviteAgentPage),
           condition: () => isManager(),
         },
       ],
