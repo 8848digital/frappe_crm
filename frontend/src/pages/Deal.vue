602618622df90c1ff78899c8ccd78a39872987ce
Shariq Ansari <sharique.rik@gmail.com>
2025-06-06 21:09:27 +0530

    refactor: statusOptions code
    (cherry picked from commit 5e616f1a50d1a6b6e4d6190f554edd704cb9b25f)

--- frontend/src/pages/Deal.vue
+++ frontend/src/pages/Deal.vue
@@ -23,15 +23,7 @@
       />
       <Dropdown
         v-if="document.doc"
-        :options="
-          statusOptions(
-            'deal',
-            updateStatus,
-            document.statuses?.length
-              ? document.statuses
-              : deal.data._customStatuses,
-          )
-        "
+        :options="statusOptions('deal', document, deal.data._customStatuses)"
       >
         <template #default="{ open }">
           <Button :label="document.doc.status">
@@ -772,10 +764,5 @@ function reloadAssignees(data) {
   if (data?.hasOwnProperty('deal_owner')) {
     assignees.reload()
   }
-}
-
-function updateStatus(value) {
-  document.doc.status = value
-  document.save.submit()
 }
 </script>
