ab4d6518fef7e3acc848b8686197beb4b6ad50b4
Shariq Ansari <sharique.rik@gmail.com>
2025-06-17 12:46:33 +0530

    fix: updated roles in Invite user page
    (cherry picked from commit 2a2c832e0bf4ca6b366fd6f98c99a2081f1d09bf)

--- frontend/src/components/Settings/InviteUserPage.vue
+++ frontend/src/components/Settings/InviteUserPage.vue
@@ -22,8 +22,9 @@
           v-model="role"
           :label="__('Invite as')"
           :options="[
-            { label: __('Regular Access'), value: 'Sales User' },
-            { label: __('Manager Access'), value: 'Sales Manager' },
+            { label: __('Sales User'), value: 'Sales User' },
+            { label: __('Manager'), value: 'Sales Manager' },
+            { label: __('Admin'), value: 'System Manager' },
           ]"
           :description="description"
         />
@@ -95,6 +96,8 @@ const error = ref(null)
 
 const description = computed(() => {
   return {
+    'System Manager':
+      'Can manage all aspects of the CRM, including user management, customizations and settings.',
     'Sales Manager':
       'Can manage and invite new agents, and create public & private views (reports).',
     'Sales User':
@@ -103,8 +106,9 @@ const description = computed(() => {
 })
 
 const roleMap = {
-  'Sales User': __('Regular Access'),
-  'Sales Manager': __('Manager Access'),
+  'Sales User': __('Sales User'),
+  'Sales Manager': __('Manager'),
+  'System Manager': __('Admin'),
 }
 
 const inviteByEmail = createResource({
