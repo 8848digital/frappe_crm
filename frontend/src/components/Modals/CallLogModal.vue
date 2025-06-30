f1d53225cdae5f532cc45c872612b49d377e9f67
Shariq Ansari <sharique.rik@gmail.com>
2025-06-30 18:43:19 +0530

    fix: added on before create hook in call log modal
    (cherry picked from commit ac13b7a3bd58021a91793817f07b4703bd8f42b4)

--- frontend/src/components/Modals/CallLogModal.vue
+++ frontend/src/components/Modals/CallLogModal.vue
@@ -85,7 +85,7 @@ const loading = ref(false)
 const error = ref(null)
 const editMode = ref(false)
 
-const { document: callLog } = useDocument(
+const { document: callLog, triggerOnBeforeCreate } = useDocument(
   'CRM Call Log',
   props.data?.name || '',
 )
@@ -144,7 +144,7 @@ async function createCallLog() {
     telephony_medium: 'Manual',
   })
 
-  await triggerOnBeforeCreate?.(props.referenceDoc)
+  await triggerOnBeforeCreate?.()
   await _createCallLog.submit({
     doc: callLog.doc,
   })
@@ -152,16 +152,6 @@ async function createCallLog() {
 
 const _createCallLog = createResource({
   url: 'frappe.client.insert',
-  makeParams() {
-    return {
-      doc: {
-        doctype: 'CRM Call Log',
-        id: getRandom(6),
-        telephony_medium: 'Manual',
-        ...callLog.doc,
-      },
-    }
-  },
   onSuccess(doc) {
     loading.value = false
     if (doc.name) {
