59bd8c1c2be4931716ed08c89eeae335e8f2fbf3
Shariq Ansari <sharique.rik@gmail.com>
2025-07-14 20:12:52 +0530

    fix: minor fixes
    (cherry picked from commit 2584cca128628b3cb7afa8b2307bfe6e61c303c5)

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -553,3 +553,8 @@ export function TemplateOption({ active, option, theme, icon, onClick }) {
     ],
   )
 }
+
+export function copy(obj) {
+  if (!obj) return obj
+  return JSON.parse(JSON.stringify(obj))
+}
