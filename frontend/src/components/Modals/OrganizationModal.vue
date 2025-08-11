a1c677d676806799a3c38304626fca761a313a60
wasim3357 <162983182+wasim3357@users.noreply.github.com>
2025-08-11 17:42:38 +0530

    fix(Org Modal): Setting loading false in case of Validation Error
    In this case user can add the details and save the form again instead of refreshing and then trying again for fresh.
    
    (cherry picked from commit ea2e44a2befd5cfd775f7e1b0d3c2321f6d075b0)

--- frontend/src/components/Modals/OrganizationModal.vue
+++ frontend/src/components/Modals/OrganizationModal.vue
@@ -106,8 +106,10 @@ async function createOrganization() {
     },
     {
       onError: (err) => {
-        error.value = err.error?.messages?.[0]
-        loading.value = false
+        if (err.error.exc_type == 'ValidationError') {
+          error.value = err.error?.messages?.[0]
+          loading.value = false
+        }
       },
     },
   )
