4bdb298d1029866e9a565e4e87cb08dead2c3269
Shariq Ansari <sharique.rik@gmail.com>
2025-08-15 20:24:44 +0530

    fix: lead/deal status dropdown is not renderring
    (cherry picked from commit 3791e2ae70067a1be9aba1e472a497e3dff8006a)

--- frontend/src/pages/Lead.vue
+++ frontend/src/pages/Lead.vue
@@ -397,6 +397,13 @@ const statuses = computed(() => {
   return statusOptions('lead', customStatuses, triggerStatusChange)
 })
 
+const statuses = computed(() => {
+  let customStatuses = document.statuses?.length
+    ? document.statuses
+    : document._statuses || []
+  return statusOptions('lead', customStatuses, triggerStatusChange)
+})
+
 usePageMeta(() => {
   return { title: title.value, icon: brand.favicon }
 })
