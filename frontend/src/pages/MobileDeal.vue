dbf1127e90eba5951fefda61c3aa4132e55faa47
shariquerik <sharique.rik@gmail.com>
2026-02-05 17:57:14 +0530

    feat: enhance LostReasonModal to support dynamic document types and improve lost reason handling
    (cherry picked from commit 26b91e11d12ef264f14d10e1f588ad797ab02708)

--- frontend/src/pages/MobileDeal.vue
+++ frontend/src/pages/MobileDeal.vue
@@ -260,7 +260,8 @@
   <LostReasonModal
     v-if="showLostReasonModal"
     v-model="showLostReasonModal"
-    :deal="document"
+    doctype="CRM Deal"
+    :document="document"
   />
 </template>
 <script setup>
