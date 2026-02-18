8764b03860faf115aa3c849495b761d3e4a1b8ab
shariquerik <sharique.rik@gmail.com>
2026-02-18 12:27:14 +0530

    fix: remove unused run_git_command function from crm.py
    (cherry picked from commit d2f843c6d70078de8cbe693e97a05aac8e8b1b06)

--- crm/www/crm.py
+++ crm/www/crm.py
@@ -1,7 +1,5 @@
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
 # GNU GPLv3 License. See license.txt
-
-
 import os
 import subprocess
 
@@ -58,15 +56,3 @@ def get_boot():
 
 def get_default_route():
 	return "/crm"
-
-
-def run_git_command(command):
-	try:
-		with open(os.devnull, "wb") as null_stream:
-			result = subprocess.check_output(command, shell=True, stdin=null_stream, stderr=null_stream)
-		return safe_decode(result).strip()
-	except Exception:
-		frappe.log_error(
-			title="Git Command Error",
-		)
-		return ""
