90a1d04d66aeaa1b1891f461b24ab89c4a408084
shariquerik <sharique.rik@gmail.com>
2026-03-05 15:31:19 +0530

    fix: add null checks for data and viewControls in SortBy and ViewBreadcrumbs components
    (cherry picked from commit ca7a11517d80c840486561b230c012e9930c85d8)

--- frontend/src/components/ViewBreadcrumbs.vue
+++ frontend/src/components/ViewBreadcrumbs.vue
@@ -2,27 +2,35 @@
   <div class="flex items-center">
     <router-link
       :to="{ name: routeName }"
-      class="px-0.5 py-1 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-outline-gray-3 text-ink-gray-5 hover:text-ink-gray-7"
+      class="px-0.5 py-1 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-outline-gray-3"
+      :class="[
+        viewControls && viewControls.viewsDropdownOptions
+          ? 'text-ink-gray-5 hover:text-ink-gray-7'
+          : 'text-ink-gray-7',
+      ]"
     >
       {{ __(routeName) }}
     </router-link>
     <span
-      v-if="viewControls"
+      v-if="viewControls && viewControls.viewsDropdownOptions"
       class="mx-0.5 text-base text-ink-gray-4"
       aria-hidden="true"
     >
       /
     </span>
-    <Dropdown v-if="viewControls" :options="viewControls.viewsDropdownOptions">
+    <Dropdown
+      v-if="viewControls && viewControls.viewsDropdownOptions"
+      :options="viewControls.viewsDropdownOptions"
+    >
       <template #default="{ open }">
         <Button
           variant="ghost"
           class="text-lg font-medium text-nowrap"
-          :label="__(viewControls.currentView.label)"
+          :label="__(viewControls.currentView?.label)"
           :iconRight="open ? 'chevron-up' : 'chevron-down'"
         >
           <template #prefix>
-            <Icon :icon="viewControls.currentView.icon" class="h-4" />
+            <Icon :icon="viewControls.currentView?.icon" class="h-4" />
           </template>
         </Button>
       </template>
