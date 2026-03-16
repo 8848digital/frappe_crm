cb6a76f4adb9d6354909c1e90f01cb25f6ab02ba
shariquerik <sharique.rik@gmail.com>
2026-03-16 20:24:41 +0530

    fix: update UserEmailSettings layout and improve email filtering logic
    (cherry picked from commit 5f0de16db44708d54420942aad11665ea2d38394)

--- frontend/src/components/Settings/EmailEdit.vue
+++ frontend/src/components/Settings/EmailEdit.vue
@@ -121,7 +121,7 @@ const info = {
 }
 
 const isCustomService = computed(() => {
-  return services.find((s) => s.name === props.accountData.service).custom
+  return services.find((s) => s.name === props.accountData.service)?.custom
 })
 
 const fields = computed(() => {
